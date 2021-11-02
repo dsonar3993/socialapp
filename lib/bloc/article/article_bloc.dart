8c6103804bd407ec682ec43b0e5b327d79702411
Digvijay Sonar <dsonar3993@gmail.com>
2021-11-02 17:34:27 +0530

    commit

---
+++ lib/bloc/article/article_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:socialapp/bloc/article/article_event.dart';
+import 'package:socialapp/bloc/article/article_state.dart';
+import 'package:socialapp/data/model/api_result_model.dart';
+import 'package:socialapp/data/repository/article_repository.dart';
+import 'package:meta/meta.dart';
+
+class ArticleBloc extends Bloc<ArticleEvent, ArticleState> {
+  ArticleRepository repository;
+
+  ArticleBloc({@required this.repository});
+
+  @override
+  // TODO: implement initialState
+  ArticleState get initialState => ArticleInitialState();
+
+  @override
+  Stream<ArticleState> mapEventToState(ArticleEvent event) async* {
+    if (event is FetchArticlesEvent) {
+      yield ArticleLoadingState();
+      try {
+        List<Articles> articles = await repository.getArticles();
+        yield ArticleLoadedState(articles: articles);
+      } catch (e) {
+        yield ArticleErrorState(message: e.toString());
+      }
+    }
+  }
+}
